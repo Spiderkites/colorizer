99fa9b0393c64dcae6b3a1de04ce373418a477e9
Lukas Gräßle <lukas.graessle@web.de>
2020-04-12 13:25:25 +0200

    chore(colorizer): add upload component

--- generator/src/app/modules/colorizer/colorizer.module.ts
+++ generator/src/app/modules/colorizer/colorizer.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ColorizerRoutingModule } from './colorizer-routing.module';
 import { ColorizerComponent } from './colorizer.component';
+import { UploadComponent } from './components/upload/upload.component';
 
 
 @NgModule({
-  declarations: [ColorizerComponent],
+  declarations: [ColorizerComponent, UploadComponent],
   imports: [
     CommonModule,
     ColorizerRoutingModule
