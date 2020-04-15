79bdc0781907f553f14715d02cd8bf33c308514d
Lukas Gräßle <lukas.graessle@web.de>
2020-04-15 14:14:28 +0200

    feat(evo): add evo child template

---
+++ evo-child/php/functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Eigene Smarty-Funktionen mit Vererbung aus dem Vatertemplate
+ *
+ * @copyright (c) JTL-Software-GmbH
+ * @license http://jtl-url.de/jtlshoplicense
+ *
+ * @global JTLSmarty $smarty
+ */
+
+include realpath(__DIR__ . '/../../Evo/php/functions.php');
+/*
+ * Platz für eigene Smarty-Funktionen
+ * @link http://docs.jtl-shop.de/de/latest/shop_templates/tipps_tricks.html#eigene-smarty-funktionen-integrieren
+ */
+
