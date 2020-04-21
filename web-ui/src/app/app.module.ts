7839b18f5c697bb904b1879dda005734700b2cf6
Lukas Gräßle <lukas.graessle@web.de>
2020-04-21 11:45:11 +0200

    fix(web-ui): show error toast

--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
