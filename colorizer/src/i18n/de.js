8446203497afa0d7aa3ff1273bf311ed112e8742
Lukas Gräßle <lukas.graessle@web.de>
2020-04-30 17:54:20 +0200

    chore(colorizer): add icon to clarify how to download the file

--- colorizer/src/i18n/de.js
+++ colorizer/src/i18n/de.js
@@ -13,6 +13,6 @@ export default {
     "HELP_LOAD": "Öffnen: Öffnet den zu vor gespeicherten Stand.",
     "HELP_CLEAR": "Zurücksetzen: Setzt den Modellierungsbereich auf den ursprünglichen Zustand zurück.",
     "HELP_SYMMETRICAL": "Symmetrisch: Wenn Haken gesetzt ist, werden Felder symmetrisch koloriert. Ist der Hacken nicht gesetzt, kann jedes Feld separat koloriert werden.",
-    "HELP_HOW_TO_BUY_CUSTOM_PRODUCT":`Du hast Deine Lieblingsfarbgestalltung gefunden und möchtest den Drachen so bestellen? Lade die Datei auf Dein Endgerät und schicke Sie per email an: <a href="mailto:hello@spiderkites.com?subject=custom color">hello@spiderkites.com</a>. Bestelle den Drachen, indem Du die Farbe 'custom color' in den Warenkorb legst und den Bestellvorgang abschließt. Dein Drachen geht in die Produktion. Das dauert vier bis sechs Wochen. Wir melden uns, wenn er fertig ist.`,
+    "HELP_HOW_TO_BUY_CUSTOM_PRODUCT":`Du hast Deine Lieblingsfarbgestalltung gefunden und möchtest den Drachen so bestellen? Lade die Datei (<span class="fa fa-download"></span>) auf Dein Endgerät und schicke Sie per email an: <a href="mailto:hello@spiderkites.com?subject=custom color">hello@spiderkites.com</a>. Bestelle den Drachen, indem Du die Farbe 'custom color' in den Warenkorb legst und den Bestellvorgang abschließt. Dein Drachen geht in die Produktion. Das dauert vier bis sechs Wochen. Wir melden uns, wenn er fertig ist.`,
     "HELP_CLOSE": "Schließen"
 }
