9af763e96475a681c1ff0b0c06f6de75a786c78e
Lukas Gräßle <lukas.graessle@web.de>
2020-04-30 17:18:05 +0200

    chore(colorizer): adjust i18n

--- colorizer/src/i18n/de.js
+++ colorizer/src/i18n/de.js
@@ -4,15 +4,14 @@ export default {
     "CLEAR": "Zurücksetzen",
     "LOAD": "Öffnen",
     "SYMMETRICAL": " Symmetrisch",
-    "CONFIRM_DIALOG": "Wenn Sie einen gespeicherten Stand laden, gehen nicht gespeicherte Änderungen verloren!",
+    "CONFIRM_DIALOG": "Wenn Du einen gespeicherten Stand lädst, gehen nicht gespeicherte Änderungen verloren!",
     "NO_ITEM_FOUND_DIALOG": "Es wurde keine gespeicherte Datei gefunden.",
-    
     "HELP_TITLE": "Colorizer Hilfe",
-    "HELP_DOWNLOAD": "Download: Bietet den aktuell modillierten Stand als Downlad an.",
+    "HELP_DOWNLOAD": "Download: Bietet den aktuell modellierten Stand als Downlad an.",
     "HELP_SAVE": "Speichern: Speichert den aktuell modellierten Stand im Browser zur späteren Weiterbearbeitung.",
     "HELP_LOAD": "Öffnen: Öffnet den zu vor gespeicherten Stand.",
     "HELP_CLEAR": "Zurücksetzen: Setzt den Modellierungsbereich auf den ursprünglichen Zustand zurück.",
-    "HELP_SYMMETRICAL": "Symmetrisch: Wenn Hacken gesetzt ist, werden Felder symmetrisch Koloriert. Ist der Hacken nicht gesetzt, kann jedes Feld separat Koloriert werden.",
-    "HELP_HOW_TO_BUY_CUSTOM_PRODUCT":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "HELP_SYMMETRICAL": "Symmetrisch: Wenn Haken gesetzt ist, werden Felder symmetrisch koloriert. Ist der Hacken nicht gesetzt, kann jedes Feld separat koloriert werden.",
+    "HELP_HOW_TO_BUY_CUSTOM_PRODUCT":"Du hast Deine Lieblingsfarbgestalltung gefunden und möchtest den Drachen so bestellen? Lade die Datei auf Dein Endgerät und schicke Sie per email an: hello@spiderkites.com. Bestelle den Drachen, indem Du die Farbe 'custom color' in den Warenkorb legst und den Bestellvorgang abschließt. Dein Drachen geht in die Produktion. Das dauert vier bis sechs Wochen. Wir melden uns, wenn er fertig ist.",
     "HELP_CLOSE": "Schließen"
 }
--- colorizer/src/i18n/en.js
+++ colorizer/src/i18n/en.js
@@ -4,16 +4,14 @@ export default {
     "CLEAR": "Clear",
     "LOAD": "Open",
     "SYMMETRICAL": " Symmetrical",
-    "CONFIRM_DIALOG": "If you load a saved state, unsaved changes are lost!",
-    "NO_ITEM_FOUND_DIALOG": "  No saved file was found.",
-
-        
+    "CONFIRM_DIALOG": " If you load a saved file, unsaved changes will get lost!",
+    "NO_ITEM_FOUND_DIALOG": "No saved file was found.", 
     "HELP_TITLE": "Colorizer help",
-    "HELP_DOWNLOAD": "Download: Offers the currently modelled stand as a download.",
-    "HELP_SAVE": "Save:  Saves the currently modelled version in the browser for further processing later.",
-    "HELP_LOAD": "Open: Opens the previous saved version.",
-    "HELP_CLEAR": "Clear: Resets the modeling area to its original state.",
-    "HELP_SYMMETRICAL": "Symmetrical: If check mark is set, fields are colored symmetrically. If the check mark is not set, each field can be colored separately.",
-    "HELP_HOW_TO_BUY_CUSTOM_PRODUCT":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "HELP_DOWNLOAD": "Download: Downloads the currently configured version.",
+    "HELP_SAVE": "Save:  Saves the currently configured version in your browser for later editing.",
+    "HELP_LOAD": "Open: Opens the previously saved version.",
+    "HELP_CLEAR": "Clear: Resets the modeling area to its default state.",
+    "HELP_SYMMETRICAL": "Symmetrical: If checked, panels are colored symmetrically. If the check mark is not set, each panel can be colored separately.",
+    "HELP_HOW_TO_BUY_CUSTOM_PRODUCT":"You have found your favourite colour scheme and want to order the kite? Download the file to your device and send it by e-mail to: hello@spiderkites.com.Order the kite by selecting 'custom color'.  Put it in your shopping cart and proceed to check out. Your kite now goes into production. This will take four to six weeks. We will get in touch with you when it is ready.",
     "HELP_CLOSE": "Close"
 }
