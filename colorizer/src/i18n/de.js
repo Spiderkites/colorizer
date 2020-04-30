73185370cfcae41b013c6b387bed11aaeef1b895
Lukas Gräßle <lukas.graessle@web.de>
2020-04-30 14:57:25 +0200

    chore(colorizer): add i18n for help dialog

--- colorizer/src/i18n/de.js
+++ colorizer/src/i18n/de.js
@@ -6,6 +6,13 @@ export default {
     "SYMMETRICAL": " Symmetrisch",
     "CONFIRM_DIALOG": "Wenn Sie einen gespeicherten Stand laden, gehen nicht gespeicherte Änderungen verloren!",
     "NO_ITEM_FOUND_DIALOG": "Es wurde keine gespeicherte Datei gefunden.",
-    "HELP_DOWNLOAD_TO_PC": "agrgergerger"
-
+    
+    "HELP_TITLE": "Colorizer Hilfe",
+    "HELP_DOWNLOAD": "Download: Bietet den aktuell modillierten Stand als Downlad an.",
+    "HELP_SAVE": "Speichern: Speichert den aktuell modellierten Stand im Browser zur späteren Weiterbearbeitung.",
+    "HELP_LOAD": "Öffnen: Öffnet den zu vor gespeicherten Stand.",
+    "HELP_CLEAR": "Zurücksetzen: Setzt den Modellierungsbereich auf den ursprünglichen Zustand zurück.",
+    "HELP_SYMMETRICAL": "Symmetrisch: Wenn Hacken gesetzt ist, werden Felder symmetrisch Koloriert. Ist der Hacken nicht gesetzt, kann jedes Feld separat Koloriert werden.",
+    "HELP_HOW_TO_BUY_CUSTOM_PRODUCT":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "HELP_CLOSE": "Schließen"
 }
--- colorizer/src/i18n/en.js
+++ colorizer/src/i18n/en.js
@@ -6,5 +6,14 @@ export default {
     "SYMMETRICAL": " Symmetrical",
     "CONFIRM_DIALOG": "If you load a saved state, unsaved changes are lost!",
     "NO_ITEM_FOUND_DIALOG": "  No saved file was found.",
-    "HELP_DOWNLOAD_TO_PC": "agrgergerger"
+
+        
+    "HELP_TITLE": "Colorizer help",
+    "HELP_DOWNLOAD": "Download: Offers the currently modelled stand as a download.",
+    "HELP_SAVE": "Save:  Saves the currently modelled version in the browser for further processing later.",
+    "HELP_LOAD": "Open: Opens the previous saved version.",
+    "HELP_CLEAR": "Clear: Resets the modeling area to its original state.",
+    "HELP_SYMMETRICAL": "Symmetrical: If check mark is set, fields are colored symmetrically. If the check mark is not set, each field can be colored separately.",
+    "HELP_HOW_TO_BUY_CUSTOM_PRODUCT":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "HELP_CLOSE": "Close"
 }
